932c1061aee7833c1df780b12b9b8c7ea4d4eac0
peachjelly13 <ananyasharma112233441234@gmail.com>
2025-01-23 08:17:27 +0530

    extracted data according to timestamps

--- intraday/server/index.js
+++ intraday/server/index.js
@@ -1,14 +1,14 @@
 import dotenv from 'dotenv';
+import fs from 'fs';
 dotenv.config();  
 import { function_, symbol_, interval } from './constant.js';
 import apiClient from './app.js';
 
 const API_KEY = process.env.api_key;
-console.log(API_KEY)
 const fetchStockData = async (function_, symbol_, interval, API_KEY) => {
   try {
     console.log('API_KEY:', API_KEY);  
-    const response = await apiClient('/query', {  
+    const response = await apiClient('', {  
       params: {
         function: function_,
         symbol: symbol_,
@@ -16,11 +16,34 @@ const fetchStockData = async (function_, symbol_, interval, API_KEY) => {
         apikey: API_KEY,  
       }
     });
-    console.log(response.data);  
-
+    console.log("Data received",response.data);
+    return response.data;
   } catch (error) {
     console.log('Error returning data', error); 
   }
 };
 
-fetchStockData(function_, symbol_, interval, API_KEY);
+(async()=>{
+    try {
+        const OBJ = await fetchStockData(function_, symbol_, interval, API_KEY);
+        const keys = OBJ["Time Series (5min)"]
+        
+        if(keys){
+        const entities = Object.entries(keys);
+        const formattedData = entities.map(([timestamp,data])=>({
+             timestamp:timestamp,
+             ...data
+        }))
+        if(formattedData){
+            fs.writeFileSync('output.json',JSON.stringify(formattedData))
+        }}
+        else{
+            console.log("No data found")
+        }
+        } catch (error) {
+        console.log("Error occurred",error)
+        
+        }
+})();
+
+
