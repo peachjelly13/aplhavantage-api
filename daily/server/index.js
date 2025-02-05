6dd23d7f2090ee7b04c574a13fdd32775225c4d5
peachjelly13 <ananyasharma112233441234@gmail.com>
2025-02-05 11:51:15 +0530

    created function to fetch stock data

--- daily/server/index.js
+++ daily/server/index.js
@@ -7,10 +7,30 @@ dotenv.config();
 
 app.use(express.static('../public'))
 const API_KEY = process.env.api_key;
-
+// this is basically an apiClient which can be used to access the path again and again
 const apiClient = axios.create(function_,symbol,API_KEY)({
     baseURL:"https://www.alphavantage.co/query",
     headers:{
        'Content-Type': 'application/json',
     }
 })
+
+const getStockData = async(function_,symbol,API_KEY)=>{
+    //this will get the whole response from the path
+    try {
+        const response = await apiClient.get('',{
+            params:{
+                function:function_,
+                symbol:symbol,
+                api_key:API_KEY
+    
+            }
+    
+        })
+        console.log("Data Fetched")
+        return response.data;
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
